8f61d8194be375b61b23f2392eb462d257c76e1e
Andrew Lerman <aspeciale88@gmail.com>
2026-05-05 16:03:22 -0400

    build: configure uv workspace with common and jamfy packages

---
+++ packages/common/src/common/__init__.py
@@ -0,0 +1,3 @@
+"""Shared clients, models, and utilities for the macadmin-toolkit Python packages."""
+
+__version__ = "0.1.0"
---
+++ packages/jamfy/src/jamfy/__init__.py
@@ -0,0 +1,3 @@
+"""CLI for Jamf Pro automations: exports, change tracking, app installer sync."""
+
+__version__ = "0.1.0"
